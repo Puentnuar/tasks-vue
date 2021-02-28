030ff1e50a7a02e52268deddbf2d2bcd1766a9ba
puentnuar <razmadzegega@gmail.com>
2021-02-28 06:34:42 -0800

    updating code with bachend requests

--- src/App.vue
+++ src/App.vue
@@ -31,39 +31,64 @@ export default {
     toggleAddTask() {
       this.showAddTask = !this.showAddTask
     },
-    addTask(task) {
-      this.tasks = [...this.tasks, task]
+    async addTask(task) {
+      const res = await fetch('api/tasks', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(task)
+      })
+
+      const data = await res.json()
+
+      this.tasks = [...this.tasks, data]
     },
-    deleteTask(id) {
+    async deleteTask(id) {
       if(confirm('are you sure?')) {
-        this.tasks = this.tasks.filter((task) => task.id !== id)
+        const res = await fetch(`api/tasks/${id}`, {
+          method: 'DELETE'
+        })
+// check status make sure it is deleted.
+        res.status === 200 
+        ? this.tasks = this.tasks.filter((task) => task.id !== id)
+        : alert('Error Deleting task')
+
       }
     },
-    toggleReminder(id) {
-      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id)
+      const updTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+      const res = await fetch(`api/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(updTask)
+      })
+
+      const data = await res.json()
+
+      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: data.reminder} : task)
+    },
+    async fetchTasks() {
+      const res = await fetch('api/tasks')
+
+      const data = await res.json()
+
+      return data
+    },
+    async fetchTask(id) {
+      const res = await fetch(`api/tasks/${id}`)
+
+      const data = await res.json()
+
+      return data
     }
   },
-  created() {
-    this.tasks = [
-      {
-        id: 1,
-        text: 'Meeting al School',
-        day: "March 3rd at 1:30pm",
-        reminder: true,
-      },
-      {
-        id: 2,
-        text: 'Doctors Appointment',
-        day: "March 1rd at 2:30pm",
-        reminder: true,
-      },
-      {
-        id: 3,
-        text: 'Food Shopping',
-        day: "March 3rd at 11:am",
-        reminder: false,
-      }
-    ]
+  async created() {
+    this.tasks = await this.fetchTasks()
   }
 }
 </script>
--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -42,7 +42,8 @@ export default {
             }
 
             const newTask = {
-                id: Math.floor(Math.random() * 100000),
+                // id: Math.floor(Math.random() * 100000),
+                id: '',
                 text: this.text, 
                 day: this.day,
                 reminder: this.reminder
