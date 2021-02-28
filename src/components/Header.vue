e24c20a4689ff816364bbdbc4269d799a1d7ec7f
puentnuar <razmadzegega@gmail.com>
2021-02-28 08:37:47 -0800

    added vue-router and computed props

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
         <h1>{{title}}</h1>
-        <Button @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'close' : 'Add Task'" :color="showAddTask ? 'red' : 'green'"/>
+        <Button v-show="homePage" @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'close' : 'Add Task'" :color="showAddTask ? 'red' : 'green'"/>
     </header>
 </template>
 
@@ -15,8 +15,18 @@ export default {
         showAddTask: Boolean
     },
     components: {
-        Button
+        Button,
     },
+    // computed property
+    computed: {
+        homePage() {
+            if(this.$route.path === '/') {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
 }
 </script>
 
